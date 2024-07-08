c380e21f98a912317eed5909bdef6846c991ee59
PranavMukund <119150883+PranavMukund@users.noreply.github.com>
2024-07-08 14:26:31 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -16,7 +16,7 @@ export default function Login() {
   let navigate = useNavigate()
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("https://go-foodbackend-pranavs-projects-5b57fed6.vercel.app/api/loginuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
